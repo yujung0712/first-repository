62e3f877c5fd418d1f026a8594894a3212e3aa51
yujung0712 <ghddbwjd22@naver.com>
2024-09-11 15:37:45 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 def print_hello():
-    print("Hello Worold!!")
+    print("Hello World!!")
     
-if __name__ == '__main__':
-    print_hello
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+    
+if __name__=='__main__':
+    print_KIT()
+    print_hello()
