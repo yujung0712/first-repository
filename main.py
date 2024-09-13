524c20c0e93ae69629a0c047b249347dc815ccd8
yujung0712 <ghddbwjd22@naver.com>
2024-09-13 13:21:56 +0900

    main and prime_number

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import max_profit as mp
 import prime_number as pn
 
 if __name__ == '__main__':
-    pn.is_prime_number(1)
+    pn.is_prime_number(2)
     pn.is_prime_number(13)
     pn.is_prime_number(24)
     pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+    pn.is_prime_number(28)
--- prime_number.py
+++ prime_number.py
@@ -3,4 +3,4 @@ def is_prime_number(x):
         if x % num == 0:
             print("The number {num1} is not a prime number.".format(num1=x))
             return
-    print("The number {num1} is a prime number.".format(num1=x))
+    print("The number {num2} is a prime number.".format(num1=y))
